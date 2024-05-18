253ecdd80d86b33ee4eb145f584162d824c70442
arturmusalimov <arturmusalimov@gmail.com>
2024-05-18 22:41:24 +0200

    week 2 video

---
+++ week2/video_content.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+# Memoization
+
+def memo_test():
+    cache = {}
+
+    def closure(n):
+        if n in cache:
+            print("Fetching from Cache")
+            return cache[n]
+        else:
+            print("Calculating process...")
+            np.random.seed(42)
+            a = np.random.rand(n,n)
+            b = np.random.rand(n,n)
+            c = np.dot(a,b)
+            cache[n] = c
+            return c
+        
+    return closure
+
+if __name__ == "__main__":
+
+    N = input("Enter the size of the matrix: ")
+    N = int(N)
+
+    matrix_calculation = memo_test()
+    m1 = matrix_calculation(N)
+    print(m1)
+
+    N = input("Enter the size of the matrix: ")
+    N = int(N)
+    m2 = matrix_calculation(N)
+    print(m2)
+
+    N = input("Enter the size of the matrix: ")
+    N = int(N)
+    m3 = matrix_calculation(N)
+    print(m3)   
+
+    N = input("Enter the size of the matrix: ")
+    N = int(N)
+    m4 = matrix_calculation(N)
+    print(m4)   
+
+# 4 OOP basic data structures
+
+
