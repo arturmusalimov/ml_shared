0f6024216563c99d55beacad28ba7dbeeb588a59
artur <voodoomail0@gmail.com>
2024-05-23 18:33:10 +0200

    sdafsdaf

--- week2/video_content.py
+++ week2/video_content.py
@@ -1,8 +1,9 @@
 import numpy as np
+import math
 
 # Memoization
 
-def memo_test():
+""" def memo_test():
     cache = {}
 
     def closure(n):
@@ -42,8 +43,72 @@ if __name__ == "__main__":
     N = input("Enter the size of the matrix: ")
     N = int(N)
     m4 = matrix_calculation(N)
-    print(m4)   
+    print(m4)    """
 
-# 4 OOP basic data structures
+## 4 OOP and basic data structures
 
+""" def binary_search(arr, target):
+    lo = 0
+    hi = len(arr)
+    
+    while lo < hi: 
+        m = math.floor(lo+(hi-lo)/3)
+        v = arr[m]
+        
+        if v == target:
+            return True
+        if v > target:
+            hi = m
+        else: 
+            lo = m + 1
+    return False
+
+arr = [1,2,3,4,5,6,7,8,9,10,12,32,36,64,78,78,98]
+
+res = binary_search(arr,100)
+print(res) """
+
+ ## Pointers
+ 
+ 
+class Node: 
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+        
+    def createNode(self, newValue):
+        newNode = Node(newValue)
+        newNode.left = None
+        newNode.right = None
+        
+        return newNode
+    
+    def printTree(self, root): 
+        if root is None: 
+            return
+        
+        print(root.value)
+        self.printTree(root.left)
+        self.printTree(root.right)
+
+if __name__ == "__main__":
+    tree1 = BinaryTree()
+    root1 = tree1.createNode(5)
+    root1.left = tree1.createNode(3)
+    root1.left.left = tree1.createNode(12)
+    root1.left.right = tree1.createNode(4)
+    root1.right = tree1.createNode(6)
+    root1.right.right = tree1.createNode(8)
+    
+    tree1.printTree(root1)
+    
+    
+        
+
+        
 
